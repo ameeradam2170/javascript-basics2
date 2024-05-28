6ceb1287a92ce0013568c6b00ad9f5522eed8bcb
ameeradam2170 <151600957+ameeradam2170@users.noreply.github.com>
2024-05-28 12:19:52 +0530

    Add files via upload

---
+++ js 2/condition.js
@@ -0,0 +1,83 @@
+ //if(5<10)
+    // {
+  //       console.log("5 is lesser")
+    // }
+     //else{
+  //       console.log("5 is greater");
+//    }
+
+    // (5<10)?console.log("5 is lesser"):console.log("5 is greater");
+
+
+   //  let time=16;
+    // if(time>=1 && time <=11){
+   //      console.log("Good Morning")
+ //   }
+    // else if(time>=12 && time<=16){
+    //     console.log("Good Afternoon")
+   //  }
+    // else if (time>=17 &&time <=19) 
+    // {
+    //     console.log("Good Evening")
+//    } 
+    // else if  (time>=20 && time <=24)
+    // {
+   //      console.log("Good Night")   
+    // }    ;
+
+    // let number = 17;
+    // if(number % 2==0)
+ //       {
+     //        console.log(`${number } is even`)
+        // }
+   //      else{
+      //       console.log(`${number } is odd`)
+     //   }
+
+
+       //  let fruits =["Apple","Banana","Grapes","Pineapple"];
+        // for(let i=0;i<fruits.length;i++)
+     //        {
+   //             console.log(fruits[i]);
+  //          }
+//
+  //       const numbers=[1,2,3,4,5];
+  //       var sum =0;
+  //       for(let i=0;i<numbers.length;i++)
+  //          {
+  //              sum = sum+numbers[i]
+   //         
+   //         }
+  //          console.log(sum);
+
+  //for(let i=1; i<= 5;i++)
+  //  {
+ //       for ( let j=1; j<=5; j++)
+ //           {
+  //              let p =`${i}*${j} =${i*j}`
+  //              console.log(p)
+  //          }
+ //   }
+ 
+
+
+
+
+
+
+
+
+
+         
+         
+
+
+
+
+
+
+
+
+
+
+    
---
+++ js 2/loop.js
@@ -0,0 +1,14 @@
+//for(let i=1;i<=10;i++)
+ //   {
+  //      console.log(i)
+  //  }
+
+
+  let q=[1,2,3,4,5,6,7,8,9,10];
+  for(let i=0;i<q.length;i++)
+    {
+        if(q[i] % 2==0)
+    console.log(`${q[i]} is  an even`)
+else 
+console.log(`${q[i] } is  an odd`)
+    }
